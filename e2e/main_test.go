f1e60be7654704d88749160836c1942638f7894f
Amir Ahmad <4963254+Amir-Ahmad@users.noreply.github.com>
2025-08-03 12:14:33 +1000

    Add more helm tests and improve testing of cue modules

--- e2e/main_test.go
+++ e2e/main_test.go
@@ -2,7 +2,6 @@ package test
 
 import (
 	"os"
-	"path/filepath"
 	"testing"
 
 	"github.com/amir-ahmad/kogen/cmd"
@@ -15,47 +14,19 @@ func TestMain(m *testing.M) {
 	})
 }
 
-// TestScripts runs the tests in the testdata/testscripts directory
+// TestScripts runs the tests in the testdata/testscripts directory.
 func TestScripts(t *testing.T) {
-	modCacheDir := cacheDir(t)
-
 	testscript.Run(t, testscript.Params{
-		Setup: setupTestEnvironment(modCacheDir),
-		Dir:   "testdata/testscripts",
-		// Set UPDATE_GOLDEN to 0 to update golden files
+		Dir: "testdata/testscripts",
+		// Set UPDATE_GOLDEN to 0 to update golden files.
 		UpdateScripts:       os.Getenv("UPDATE_GOLDEN") == "0",
 		RequireExplicitExec: true,
 		Condition: func(cond string) (bool, error) {
-			// Set RUN_REMOTE to 0 to run remote tests
+			// Set RUN_REMOTE to 0 to run remote tests.
 			if cond == "remote" {
 				return os.Getenv("RUN_REMOTE") == "0", nil
 			}
 			return false, nil
 		},
 	})
-}
-
-// cacheDir returns the cue cache directory, this will be used to cache modules in tests
-func cacheDir(t *testing.T) string {
-	t.Helper()
-
-	if dir := os.Getenv("CUE_CACHE_DIR"); dir != "" {
-		return dir
-	}
-	dir, err := os.UserCacheDir()
-	if err != nil {
-		t.Fatalf("failed to get system cache directory: %v", err)
-	}
-	return filepath.Join(dir, "cue")
-}
-
-func setupTestEnvironment(cacheDir string) func(*testscript.Env) error {
-	return func(env *testscript.Env) error {
-		// Reuse cache directory and use my
-		env.Vars = append(env.Vars,
-			"CUE_REGISTRY=github.com/amir-ahmad=ghcr.io",
-			"CUE_CACHE_DIR="+cacheDir,
-		)
-		return nil
-	}
 }
